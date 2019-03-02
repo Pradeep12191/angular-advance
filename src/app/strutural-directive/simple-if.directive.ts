91a2aa5aea5a265abeff1c811ea5f81d453426a1
pradeep s <pradeep.s@tagitmobile.com>
2019-03-02 12:39:44 +0530

    added else template reference to simple if directive

--- src/app/strutural-directive/simple-if.directive.ts
+++ src/app/strutural-directive/simple-if.directive.ts
@@ -4,17 +4,26 @@ import { Directive, Input, ViewContainerRef, TemplateRef } from "@angular/core";
     selector: '[appSimpleIf]'
 })
 export class SimpleIfDirective {
-
+    private _elseTempRef: TemplateRef<any>;
     constructor(
         private vc: ViewContainerRef,
         private templateRef: TemplateRef<any>
     ) {
 
     }
+
+    @Input() set appSimpleIfElse(temp) {
+        this._elseTempRef = temp;
+    }
+
     @Input() set appSimpleIf(condition) {
         this.vc.clear();
         if (condition) {
             this.vc.createEmbeddedView(this.templateRef);
+        } else {
+            if (this._elseTempRef) {
+                this.vc.createEmbeddedView(this._elseTempRef);
+            }
         }
     }
 }
--- src/app/strutural-directive/strutural-directive.component.ts
+++ src/app/strutural-directive/strutural-directive.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class StruturalDirectiveComponent implements OnInit {
 
   show = false;
+  showTemplate = false;
   constructor() { }
 
   ngOnInit() {
