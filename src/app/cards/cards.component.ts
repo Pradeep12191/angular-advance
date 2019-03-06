4ca6ab4bc48769b61234e5788a32e6e58aeeae77
pradeep s <pradeep.s@tagitmobile.com>
2019-03-06 11:55:59 +0530

    card with flip animation

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -33,86 +33,24 @@ export class CardsComponent implements OnInit {
         'ty': 'detailscard',
         'intial': 'true',
         'list': [{
-          'text1': 'Name',
-          'text2': 'rama'
+          'label': 'Name',
+          'value': 'rama'
         },
         {
-          'text1': 'Email ID',
-          'text2': 'rama@gamil.com'
+          'label': 'Email ID',
+          'value': 'rama@gamil.com'
         },
         {
-          'text1': 'Contact No',
-          'text2': '999232676'
+          'label': 'Contact No',
+          'value': '999232676'
         }
         ],
         'image': {
-          'imgPath': 'assets/images',
-          'imgName': 'hand shake.png'
         },
         'header': 'Know your RM',
-        'linkInfo': [{
-          'linkLabel': 'Set as recurring',
-          'linkAction': ''
-        }],
+        'linkInfo': [],
         'buttonListTile': [{
-          'buttonName': 'KNOW YOUR RM',
-          'buttonId': 'Edit',
-          'buttonStyle': 'primary',
-          'buttonAction': '',
-          'buttonType': 'C',
-          'buttonActive': ''
-        }]
-      }
-    },
-    {
-      'front_face_card': {
-        'ty': 'imagecard',
-        'list': [],
-        'image': {
-          'imgPath': 'assets/images',
-          'imgName': 'hand shake.png'
-        },
-        'header': 'Reach your relationship Manager',
-        'linkInfo': [{
-          'linkLabel': 'Set as recurring',
-          'linkAction': ''
-        }],
-        'buttonListTile': [{
-          'buttonName': 'KNOW YOUR RM',
-          'buttonId': 'Edit',
-          'buttonStyle': 'primary',
-          'buttonAction': '',
-          'buttonType': 'C',
-          'buttonActive': ''
-        }]
-      },
-      'back_face_card': {
-        'ty': 'detailscard',
-        'intial': 'true',
-        'list': [{
-          'text1': 'Name',
-          'text2': 'rama'
-        },
-        {
-          'text1': 'Email ID',
-          'text2': 'rama@gamil.com'
-        },
-        {
-          'text1': 'Contact No',
-          'text2': '999232676'
-        }
-        ],
-        'image': {
-          'imgPath': 'assets/images',
-          'imgName': 'hand shake.png'
-        },
-        'header': 'Reach your relationship Manager',
-        'linkInfo': [{
-          'linkLabel': 'Set as recurring',
-          'linkAction': ''
-        }],
-        'buttonListTile': [{
-          'buttonName': 'KNOW YOUR RM',
+          'buttonName': 'OK',
           'buttonId': 'Edit',
           'buttonStyle': 'primary',
           'buttonAction': '',
--- src/app/cards/multiplecard/multiplecard.component.ts
+++ src/app/cards/multiplecard/multiplecard.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+const rotate = trigger('rotateState', [
+  state('front', style({
+    transform: 'rotateY(0deg)'
+  })),
+  state('back', style({
+    transform: 'rotateY(180deg)'
+  })),
+  transition('front => back', animate(500)),
+  transition('back => front', animate(500))
+]);
 
 @Component({
   selector: 'app-multiplecard',
   templateUrl: './multiplecard.component.html',
-  styleUrls: ['./multiplecard.component.scss']
+  styleUrls: ['./multiplecard.component.scss'],
+  animations: [rotate]
 })
 export class MultiplecardComponent implements OnInit {
-  public visibleCard;
+  public frontCard;
+  public backCard;
+  public direction: 'front' | 'back';
   @Input() card;
 
   ngOnInit() {
-    this.visibleCard = this.card['front_face_card'];
+    this.frontCard = this.card['front_face_card'];
+    this.backCard = this.card['back_face_card'];
   }
 
-  toggleCard() {
-    
+  rotate(direction) {
+    this.direction = direction;
   }
 }
