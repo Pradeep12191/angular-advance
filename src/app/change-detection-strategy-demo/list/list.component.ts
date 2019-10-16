998aae6b88502846299ab3cf9d4dea4719f5d9b0
pradeep s <pradeep.s@tagitmobile.com>
2019-10-16 19:09:20 +0530

    sample commit

--- src/app/change-detection-strategy-demo/list/list.component.ts
+++ src/app/change-detection-strategy-demo/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Sample } from 'test/model';
+import { Detail } from 'models/ComponentDetail';
+import { JSON_COMPONENT } from 'data/constant';
+import { OE_Component_Details } from 'data/OE_data/components/component_details';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -8,5 +12,9 @@ import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent {
-    @Input() items: any[];
+    @Input() items: Sample[];
+    constructor() {
+        const a: Sample = { test: 'hello' };
+        console.log(a);
+    }
 }
